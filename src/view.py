e48ae8ee735fbd06849a3e9e15394b6567e106c1
johnathanleif <johnathanleif@hostlocal>
2021-11-12 16:35:47 +0000

    cleaned ui code and removed left over print statements

--- src/view.py
+++ src/view.py
@@ -3,56 +3,50 @@ from tkinter import ttk
 
 
 WINDOW_TITLE = "Formulae Calculator"
-FORMULA_ENTRY_LABEL = "Input Formula:"
-PARSE_BUTTON_LABEL = "Parse" 
-CALC_BUTTON_LABEL = "Calculate"
-RESULT_LABEL = "Result = "
+FORMULA_ENTRY_TEXT = "Input Formula: "
+PARSE_BUTTON_TEXT = "Parse" 
+CALC_BUTTON_TEXT = "Calculate"
+VAR_TEXT = " = "
+RESULT_TEXT = "Result = "
 
 ENTRY_BG = "white"
 ENTRY_FG = "black"
 
 VARIABLE_ENTRY_COLUMNS = 2
 
-class TkView:
+class TkView(tk.Tk):
 
     def __init__(self, parse_observer, calculate_observer):
+        super().__init__(None)
+        self.title(WINDOW_TITLE)
         self.parse_observer = parse_observer
         self.calculate_observer = calculate_observer
-        self.root = tk.Tk()
-        self.root.title(WINDOW_TITLE)
-
 
 
     def open_window(self):
         self.create_formula_input()
-        self.root.mainloop()
+        self.mainloop()
 
 
     def create_formula_input(self):
-        frame = tk.Frame(self.root)
+        frame = tk.Frame(self)
         frame.pack()
 
-        entry_label = tk.Label(frame, text = FORMULA_ENTRY_LABEL)
+        entry_label = tk.Label(frame, text = FORMULA_ENTRY_TEXT)
         entry = tk.Entry(frame, bg = ENTRY_BG, fg = ENTRY_FG)
-        parse_button = tk.Button(frame, text = PARSE_BUTTON_LABEL, command = lambda: self.parse(entry.get()))
+        parse_button = tk.Button(frame, text = PARSE_BUTTON_TEXT, command = lambda: self.parse(entry.get()))
 
         entry_label.pack(side = tk.LEFT)
         entry.pack(side = tk.LEFT)
         parse_button.pack(side = tk.RIGHT)
 
-    def parse(self, input_formula_string):
-        self.parse_observer.notify(input_formula_string)
+        self.resize()
 
-
-    def create_separator(self):
-        separator = ttk.Separator(self.root)
-        separator.pack(fill = 'x')
     
-
     def create_variable_input(self, variables):
         self.create_separator()
 
-        variable_frame = tk.Frame(self.root)
+        variable_frame = tk.Frame(self)
         variable_frame.pack()
     
         variables_grid_frame = tk.Frame(variable_frame)
@@ -70,7 +64,7 @@ class TkView:
                 col = 0
                 row += 1
 
-            entry_label = tk.Label(frame, text = var)
+            entry_label = tk.Label(frame, text = var + VAR_TEXT) 
             entry = tk.Entry(frame, bg = ENTRY_BG, fg = ENTRY_FG)
 
             entry_label.pack(side = tk.LEFT)
@@ -79,10 +73,38 @@ class TkView:
             var_entries[var] = entry
 
         
-        calc_button = tk.Button(variable_frame, text = CALC_BUTTON_LABEL, command = lambda: self.calculate(self.get_var_entry_input(var_entries)))
+        calc_button = tk.Button(variable_frame, text = CALC_BUTTON_TEXT, command = lambda: self.calculate(self.get_var_entry_input(var_entries)))
         calc_button.pack()
 
-        self.root.geometry("")
+        self.resize()
+
+
+    def display_result(self, result):
+        self.create_separator()
+        frame = tk.Frame(self)
+        frame.pack()
+        
+        label = tk.Label(frame, text = RESULT_TEXT + result)
+        label.pack()
+        
+        self.resize()
+ 
+    
+    def create_separator(self):
+        separator = ttk.Separator(self)
+        separator.pack(fill = 'x')
+    
+    
+    def resize(self):
+        self.geometry('')
+    
+
+    def parse(self, input_formula_string):
+        self.parse_observer.notify(input_formula_string)
+
+
+    def calculate(self, var_inputs):
+        self.calculate_observer.notify(var_inputs)    
 
 
     def get_var_entry_input(self, var_entries):
@@ -90,17 +112,5 @@ class TkView:
         for var in var_entries:
             var_inputs[var] = var_entries[var].get() 
 
-        return var_inputs
-
-
-    def calculate(self, var_inputs):
-        self.calculate_observer.notify(var_inputs)    
-
-
-    def display_result(self, result):
-        frame = tk.Frame(self.root)
-        frame.pack()
-        
-        label = tk.Label(frame, text = RESULT_LABEL + result)
-        label.pack()
-
+        return var_inputs    
+   
